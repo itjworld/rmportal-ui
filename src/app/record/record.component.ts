5279bb030d70c358a98987bee865d891e60de0a5
Ashish <ashish9.mca@gmail.com>
2018-01-17 18:50:11 +0530

    add print feature in data-grid

--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -107,6 +107,12 @@ export class RecordComponent {
     }
   }
 
-    
+  print(){
+    var innerContents = document.getElementById("printSectionId").innerHTML;
+    var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
+    popupWinindow.document.open();
+    popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="assets/css/record.component.css" /></head><body onload="window.print()">' + innerContents + '</html>');
+    popupWinindow.document.close();
+  } 
   
 }
